a357b14c2c0e33c4511c58870777b846c96f7198
ymiftah <miftahyo@outlook.fr>
2020-05-31 15:32:53 +0800

    aws storage keys

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', None)
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', None)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'myportfolio-youssefm'
 AWS_S3_REGION_NAME = 'ap-southeast-1'
